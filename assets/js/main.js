e6e2f9d7429e0f83a6abc5312059406734a86653
tGoh98 <tmg5@rice.edu>
2019-06-12 21:31:16 -0500

    changed api call to async function
    in order to use await so data can update properly

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ new Vue({
     ]
   },
   methods: {
-   search: function () {
+   search: async function () {
      // Get form values
      let stateCode = this.selectedState.value
      let designation = this.selectedDesignation.value
@@ -100,12 +100,17 @@ new Vue({
 
      // API call
      const apiKey = 'GvdIIgwFiaoPxjBJSUlSedvsGCcUMGBCcoQOLs33';
-     axios.get(`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&q=${q}&api_key=${apiKey}`).then(response => (this.info = response))
+     this.info = await axios.get(`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&q=${q}&api_key=${apiKey}`).then(response => (this.info = response.data.data)).catch(error => {
+        console.log(error)
+        this.errored = true
+      })
      console.log(this.info);
+     console.log(this.info.length); //doesn't work for null obj, see if some param that works
 
      // Filter results by selected designation
      if(designation != "Any") {
-       // Filter this.info
+       // Filter this.info with array.filter
+
      }
 
      // Show results
