ec7091087cee3815eba5fadbd9cb265d5628b235
tGoh98 <tmg5@rice.edu>
2019-06-17 21:39:08 -0500

    moved dummy data constant to a separate file

---
+++ assets/js/data.js
@@ -0,0 +1,108 @@
+// Dummy info for gallery for initial page load
+let data = [
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  },
+  {
+    "images": [
+      {
+        "url": "https://www.nps.gov/common/uploads/structured_data/3C7AAC04-1DD8-B71B-0B6534785C41D6B5.jpg"
+      }
+    ],
+  }
+]
